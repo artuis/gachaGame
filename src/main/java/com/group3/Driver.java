f3d0d7943348e90561a92ed3111ced79725b02bb
sctallman <sctallman@outlook.com>
2021-05-03 18:28:05 -0400

    driver main added - will it blend?

--- src/main/java/com/group3/Driver.java
+++ src/main/java/com/group3/Driver.java
@@ -1,9 +1,11 @@
 package com.group3;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Driver {
-
-	public Driver() {
-		// TODO Auto-generated constructor stub
+	public static void main(String[] args) {
+		SpringApplication.run(Driver.class, args);
 	}
-
 }
