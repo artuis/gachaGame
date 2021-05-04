fbdfdf5b34d4fe7bc2e855dd7d80590f76b132b6
sctallman <sctallman@outlook.com>
2021-05-04 11:55:04 -0400

    repos have basic functionality set, application.conf file valid for accessing sctallman's aws keyspaces, application.conf removed from gitignore for this reason

--- src/main/java/com/group3/data/EventRepository.java
+++ src/main/java/com/group3/data/EventRepository.java
@@ -1,6 +1,14 @@
 package com.group3.data;
 
-public interface EventRepository {
+
+import java.util.List;
+
+import org.springframework.data.cassandra.repository.AllowFiltering;
+import org.springframework.data.cassandra.repository.ReactiveCassandraRepository;
+
+import com.group3.beans.Event;
+
+public interface EventRepository extends ReactiveCassandraRepository<Event, /*PRIMARY KEY DATATYPE REPLACES String PLACEHOLDER*/ String> {
 
 
 }
