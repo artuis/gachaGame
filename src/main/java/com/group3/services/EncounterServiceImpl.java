3b1b89e554dc827e75f2fe9e9309be7bcdb61dd2
lwood.benjamin <benjamin.wood@revature.net>
2021-05-05 08:50:49 -0500

    poking around with Async

--- src/main/java/com/group3/services/EncounterServiceImpl.java
+++ src/main/java/com/group3/services/EncounterServiceImpl.java
@@ -6,9 +6,13 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
 import com.group3.beans.Collectible;
 import com.group3.beans.Encounter;
 
+@Service
 public class EncounterServiceImpl implements EncounterService {
 
 	public EncounterServiceImpl() {
@@ -16,7 +20,8 @@ public class EncounterServiceImpl implements EncounterService {
 	}
 
 	@Override
-	public Future<?> runEncounter(List<Collectible> sent, Encounter journey) {
+	@Async
+	public static Future<?> runEncounter(List<Collectible> sent, Encounter journey) {
 		// The deterministic version of the encounter runner.
 		// A random version can be made later. (this is simpler to test)
 		
@@ -26,7 +31,7 @@ public class EncounterServiceImpl implements EncounterService {
 						.mapToInt(x -> x.getStatSum())
 						.sum();
 		int reward;
-		
+
 		// If the combined statistical score of each, reaches the set
 		// Encounter difficulty, it wins and returns with a reward.
 		if(total >= journey.getDifficulty()) {
@@ -40,13 +45,11 @@ public class EncounterServiceImpl implements EncounterService {
 		}
 		
 		// Create and return Future
-		ExecutorService es = Executors.newSingleThreadExecutor();
-		Future<Integer> futureReward = es.submit(() -> {
+		Future<Integer> futureReward = () -> {
 			Thread.sleep(journey.getLength());
 			return reward;
-		});
+		};
 		
 		return futureReward;
 	}
-
 }
