a256a0ff8cbbd4be30e5354f43efa13fb9e32af6
Tony Touma <chielo9513@protonmail.com>
2021-05-08 13:23:46 -0400

    Event bean developed

--- src/main/java/com/group3/beans/Event.java
+++ src/main/java/com/group3/beans/Event.java
@@ -1,9 +1,96 @@
 package com.group3.beans;
 
+import java.util.Date;
+
+import org.springframework.data.cassandra.core.mapping.Column;
+import org.springframework.data.cassandra.core.mapping.PrimaryKey;
+import org.springframework.data.cassandra.core.mapping.Table;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+@Table("events")
 public class Event {
+	
+	@Column
+	@JsonInclude(Include.NON_NULL)
+	private boolean isOngoing;
+	
+	@Column
+	@JsonInclude(Include.NON_NULL)
+	private Date eventStart;
+	
+	@Column
+	@PrimaryKey
+	@JsonInclude(Include.NON_NULL)
+	private Date eventEnd;
 
 	public Event() {
 		// TODO Auto-generated constructor stub
 	}
 
+	public boolean isOngoing() {
+		return isOngoing;
+	}
+
+	public void setOngoing(boolean isOngoing) {
+		this.isOngoing = isOngoing;
+	}
+
+	public Date getEventStart() {
+		return eventStart;
+	}
+
+	public void setEventStart(Date eventStart) {
+		this.eventStart = eventStart;
+	}
+
+	public Date getEventEnd() {
+		return eventEnd;
+	}
+
+	public void setEventEnd(Date eventEnd) {
+		this.eventEnd = eventEnd;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((eventEnd == null) ? 0 : eventEnd.hashCode());
+		result = prime * result + ((eventStart == null) ? 0 : eventStart.hashCode());
+		result = prime * result + (isOngoing ? 1231 : 1237);
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Event other = (Event) obj;
+		if (eventEnd == null) {
+			if (other.eventEnd != null)
+				return false;
+		} else if (!eventEnd.equals(other.eventEnd))
+			return false;
+		if (eventStart == null) {
+			if (other.eventStart != null)
+				return false;
+		} else if (!eventStart.equals(other.eventStart))
+			return false;
+		if (isOngoing != other.isOngoing)
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Event [isOngoing=" + isOngoing + ", eventStart=" + eventStart + ", eventEnd=" + eventEnd + "]";
+	}
+
+	
 }
