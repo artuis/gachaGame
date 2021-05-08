a1e4c883f29672b186b448575d35f6f3beee444b
Tony Touma <chielo9513@protonmail.com>
2021-05-08 13:27:10 -0400

    EventRepo datatype Date replaces String placeholder

--- src/main/java/com/group3/data/EventRepository.java
+++ src/main/java/com/group3/data/EventRepository.java
@@ -1,14 +1,13 @@
 package com.group3.data;
 
 
-import java.util.List;
+import java.util.Date;
 
-import org.springframework.data.cassandra.repository.AllowFiltering;
 import org.springframework.data.cassandra.repository.ReactiveCassandraRepository;
 
 import com.group3.beans.Event;
 
-public interface EventRepository extends ReactiveCassandraRepository<Event, /*PRIMARY KEY DATATYPE REPLACES String PLACEHOLDER*/ String> {
+public interface EventRepository extends ReactiveCassandraRepository<Event, /*PRIMARY KEY DATATYPE REPLACES String PLACEHOLDER*/ Date> {
 
 
 }
