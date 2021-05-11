2e307f97e0e81c5781f3c6fe084c862f108892cf
Tony Touma <chielo9513@protonmail.com>
2021-05-11 17:54:24 -0400

    fixed unused vars

--- src/main/java/com/group3/data/EncounterRepository.java
+++ src/main/java/com/group3/data/EncounterRepository.java
@@ -1,8 +1,6 @@
 package com.group3.data;
 
-import java.util.List;
 
-import org.springframework.data.cassandra.repository.AllowFiltering;
 import org.springframework.data.cassandra.repository.ReactiveCassandraRepository;
 
 import com.group3.beans.Encounter;
