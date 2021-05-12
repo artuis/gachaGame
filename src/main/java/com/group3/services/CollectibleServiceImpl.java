59cbc0f4985543702776fddd29009b6e1ead8114
sctallman <sctallman@outlook.com>
2021-05-12 10:31:11 -0400

    bug fix

--- src/main/java/com/group3/services/CollectibleServiceImpl.java
+++ src/main/java/com/group3/services/CollectibleServiceImpl.java
@@ -118,7 +118,7 @@ public class CollectibleServiceImpl implements CollectibleService {
 		List<Collectible> collectibles = getAllCollectibles().filter(collectible -> {
 			return collectibleIds.contains(collectible.getId());
 		}).collectList().block();
-		if(collectibles.isEmpty()) {
+		if(collectibles == null | collectibles.isEmpty()) {
 			return Mono.empty();
 		}
 		// if the collectibles in the list aren't all of the same type, return empty
