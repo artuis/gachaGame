815558e4899e79d6c2e4b71139b1eecc53688d2b
Thomas An <tommyan97@msn.com>
2021-05-14 13:00:15 -0700

    removing some things

--- src/main/java/com/group3/Driver.java
+++ src/main/java/com/group3/Driver.java
@@ -2,8 +2,6 @@ package com.group3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.FilterType;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 
